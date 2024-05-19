1bd4d3a44fa92ce4fc7db37ea86eccd5b6218635
AlberttiCaleffo <albertticaleffo97@gmail.com>
2024-05-18 23:31:25 -0300

    Capitulo 3

---
+++ capitulo_2/cumprimentos.py
@@ -0,0 +1,5 @@
+nomes = ['vinicius', 'guilherme', 'gabriel']
+
+print(f'Ola {nomes[0].title()}, seja bem-vindo!')
+print(f'Ola {nomes[1].title()}, seja bem-vindo!')
+print(f'Ola {nomes[2].title()}, seja bem-vindo!')
---
+++ capitulo_2/sua_lista.py
@@ -0,0 +1,5 @@
+veiculos = ['moto', 'carro', 'bicicleta']
+
+print(f'Gostaria de uma {veiculos[0]} da Honda.')
+print(f'Gostaria de um {veiculos[1]} Ford.')
+print(f'Gostaria de uma {veiculos[2]} de carbono.')
